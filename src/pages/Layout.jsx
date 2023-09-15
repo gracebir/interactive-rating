ffb18bd3d2cdedc4cf79576f9f51875dadcb05f0
gracebir <grace.devolop@gmail.com>
2023-09-15 19:43:31 +0200

    adjust layout

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Layout = ({children}) => {
   return (
-    <div className="max-w-md mx-auto border flex-1 bg-dark-blue">
+    <div className="max-w-md mx-auto flex-1 bg-dark-blue rounded-3xl p-8">
       {children}
     </div>
   )
