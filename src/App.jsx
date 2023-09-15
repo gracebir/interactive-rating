6a92badec9d7e4071edb723ea2389eb3f7881edd
gracebir <grace.devolop@gmail.com>
2023-09-15 19:44:39 +0200

    adding padding

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom"
 import Rate from "./pages/Rate"
 import Summary from "./pages/Summary"
+import { useState } from "react"
 
 function App() {
+  const [rate, setRate] = useState(0)
   return (
     <div className="font-overpass h-screen bg-dark w-full flex items-center px-6 lg:px-0">
       <Routes>
-        <Route element={<Rate/>} path="/"/>
-        <Route element={<Summary/>} path="/summary"/>
+        <Route element={<Rate rate={rate} setRate={setRate}/>} path="/"/>
+        <Route element={<Summary rate={rate}/>} path="/summary"/>
       </Routes>
     </div>
   )
